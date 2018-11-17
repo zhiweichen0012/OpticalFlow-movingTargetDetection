d8641c74cc8dc21f114386dbb07c081cfe15b99e
zhiweichen <30588754+zhiweichen12@users.noreply.github.com>
2018-11-17 20:30:27 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/opticalFlow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opticalFlow.dir/main.cpp.o"
+  "opticalFlow.pdb"
+  "opticalFlow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opticalFlow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
